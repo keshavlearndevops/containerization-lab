f6a18d664af5dc0fd41ef83160192f20dab2ad8b
Keshav Kumar Bathla <keshav.bathla@hrone.cloud>
2026-05-19 12:17:07 +0530

    updated path

--- Todo-python/backend/app.py
+++ Todo-python/backend/app.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-DATA_FILE = os.environ.get("DATA_FILE", "data/todos.json")
+DATA_FILE = os.environ.get("DATA_FILE", "/app/data/todos.json")
 
 
 def load():
